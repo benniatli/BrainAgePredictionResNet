b547b59a39225218df52fbcb7a8db144c149b76d
Benedikt Atli Jónsson <benniaj@gmail.com>
2022-03-04 13:54:40 +0000

    Added CAT12 settings
    Added a file containing the CAT12 settings used for MRI preprocessing.

---
+++ Code/CAT12_brain_age_parameters.m
@@ -0,0 +1,23 @@
+matlabbatch{1}.spm.tools.cat.estwrite.data = {'inputFileHere,1'};
+matlabbatch{1}.spm.tools.cat.estwrite.nproc = 0;
+matlabbatch{1}.spm.tools.cat.estwrite.opts.tpm = {'/spm12/tpm/TPM.nii'};
+matlabbatch{1}.spm.tools.cat.estwrite.opts.affreg = 'mni';
+matlabbatch{1}.spm.tools.cat.estwrite.extopts.APP = 1;
+matlabbatch{1}.spm.tools.cat.estwrite.extopts.LASstr = 0.5;
+matlabbatch{1}.spm.tools.cat.estwrite.extopts.gcutstr = 0.5;
+matlabbatch{1}.spm.tools.cat.estwrite.extopts.cleanupstr = 0.5;
+matlabbatch{1}.spm.tools.cat.estwrite.extopts.darteltpm = {'/spm12/toolbox/cat12/templates_1.50mm/Template_1_IXI555_MNI152.nii'};
+matlabbatch{1}.spm.tools.cat.estwrite.extopts.vox = 1.5;
+matlabbatch{1}.spm.tools.cat.estwrite.output.ROI = 1;
+matlabbatch{1}.spm.tools.cat.estwrite.output.surface = 0;
+matlabbatch{1}.spm.tools.cat.estwrite.output.GM.native = 0;
+matlabbatch{1}.spm.tools.cat.estwrite.output.GM.mod = 1;
+matlabbatch{1}.spm.tools.cat.estwrite.output.GM.dartel = 0;
+matlabbatch{1}.spm.tools.cat.estwrite.output.WM.native = 0;
+matlabbatch{1}.spm.tools.cat.estwrite.output.WM.mod = 1;
+matlabbatch{1}.spm.tools.cat.estwrite.output.WM.dartel = 0;
+matlabbatch{1}.spm.tools.cat.estwrite.output.bias.warped = 1;
+matlabbatch{1}.spm.tools.cat.estwrite.output.jacobian.warped = 1;
+matlabbatch{1}.spm.tools.cat.estwrite.output.warps = [0 0];"
+
+
